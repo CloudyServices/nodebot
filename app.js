aa7e23024ccff003a35c01883722914fccf09a9c
CloudyServices <info@cloudyservices.co.uk>
2017-06-22 14:41:43 +0100

    Errors sorted

---
+++ app.js
@@ -0,0 +1,30 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=============
+// Bot Setup
+//
+
+//Setup Restify server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+//Create chat bot
+var connector = new builder.ChatConnector({
+    appID: '',
+    appPassword: ''
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//===========
+// Bots Dialogs
+//===========
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
+
+
